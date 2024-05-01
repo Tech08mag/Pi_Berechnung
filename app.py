e8d285a62eec7048b70123e4a8a45bae01bd576c
Tech08mag <tech08mag@gmail.com>
2024-05-01 11:05:12 +0200

    OOP

--- app.py
+++ app.py
@@ -1,23 +1,27 @@
 import decimal
 
-def pi_gauss_legendre(precision):
-    decimal.getcontext().prec = precision + 2
-    a = decimal.Decimal(1)
-    b = 1 / decimal.Decimal(2).sqrt()
-    t = decimal.Decimal(1) / decimal.Decimal(4)
-    p = decimal.Decimal(1)
+class Pi():
+    def get_precision():
+        precision = int(input("Gebe die Nachkommastellen von Pi ein, die du berechnen möchtest: "))
+        return precision
+    
+    def pi_gauss_legendre():
+        precision = Pi.get_precision()
+        decimal.getcontext().prec = precision + 2
+        a = decimal.Decimal(1)
+        b = 1 / decimal.Decimal(2).sqrt()
+        t = decimal.Decimal(1) / decimal.Decimal(4)
+        p = decimal.Decimal(1)
+    
+        for _ in range(precision):
+            a_next = (a + b) / 2
+            b = (a * b).sqrt()
+            t -= p * (a - a_next) ** 2
+            a = a_next
+            p *= 2
+    
+        pi_approx = (a + b) ** 2 / (4 * t)
+        return pi_approx
 
-    for _ in range(precision):
-        a_next = (a + b) / 2
-        b = (a * b).sqrt()
-        t -= p * (a - a_next) ** 2
-        a = a_next
-        p *= 2
-
-    pi_approx = (a + b) ** 2 / (4 * t)
-    return pi_approx
-
-# Berechnen von π mit 100 Nachkommastellen Genauigkeit
-precision = 100
-pi_approx = pi_gauss_legendre(precision)
+pi_approx = Pi.pi_gauss_legendre()
 print(pi_approx)
